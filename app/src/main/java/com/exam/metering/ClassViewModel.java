d83b18e5f832451d876e0c9cad77c31b521868bc
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2021-11-04 22:27:18 +0900

    create: class DB and categoryView

---
+++ app/src/main/java/com/exam/metering/ClassViewModel.java
@@ -0,0 +1,4 @@
+package com.exam.metering;
+
+public class ClassViewModel {
+}
---
+++ app/src/main/java/com/exam/metering/category.java
@@ -0,0 +1,14 @@
+package com.exam.metering;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
