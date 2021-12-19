74f0e7ddac0f0629bbf7ad33fb4f31b144520e7f
yn15 <yn15@naver.com>
2021-12-20 00:26:46 +0900

    class, layout rename

--- app/src/main/java/com/exam/metering/PaySummaryFragment.java
+++ app/src/main/java/com/exam/metering/PaySummaryFragment.java
@@ -58,6 +58,6 @@ public class PaySummaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reg_summary, container, false);
+        return inflater.inflate(R.layout.fragment_pay_summary, container, false);
     }
 }
